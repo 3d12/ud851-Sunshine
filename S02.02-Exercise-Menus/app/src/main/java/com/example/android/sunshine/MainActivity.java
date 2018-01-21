8782e8be9aba3f2f3f8091b6f7fbf365f5313003
Nick E <3d12@users.noreply.github.com>
2018-01-20 23:10:34 -0600

    S02.02 Completed

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,9 +15,14 @@
  */
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -97,12 +102,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called forecast.xml
-    // TODO (3) Add one item to the menu with an ID of action_refresh
-    // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
+    // DONE (2) Create a menu resource in res/menu/ called forecast.xml
+    // DONE (3) Add one item to the menu with an ID of action_refresh
+    // DONE (4) Set the title of the menu item to "Refresh" using strings.xml
 
-    // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
-    // TODO (6) Return true to display the menu
+    // DONE (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+    // DONE (6) Return true to display the menu
 
-    // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+
+    // DONE (7) Override onOptionsItemSelected to handle clicks on the refresh button
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClicked = item.getItemId();
+        if (itemThatWasClicked == R.id.action_refresh) {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
