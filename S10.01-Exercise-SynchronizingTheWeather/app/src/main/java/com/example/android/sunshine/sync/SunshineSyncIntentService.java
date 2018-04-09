de27b0fffdd93e00b2b949b8e0e5ef8fd9490c09
Nick E <3d12@users.noreply.github.com>
2018-04-09 15:12:51 -0500

    S10.01 Completed This exercise helped cement my understanding of this concept. I was still a little bit fuzzy on the details of implementation for this concept, but being able to explore a bit more free-form in this exercise helped that understanding, I think.

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,31 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+// DONE (5) Create a new class called SunshineSyncIntentService that extends IntentService
+//  DONE (6) Create a constructor that calls super and passes the name of this class
+//  DONE (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+public class SunshineSyncIntentService extends IntentService {
+    public static final String ACTION_REFRESH_DATA_FROM_SOURCE = "action-refresh-data-from-source";
+
+    public SunshineSyncIntentService() {
+        super("SunshineSyncIntentService");
+        Log.d("SunshineIntentService", "Service was successfully created via default constructor");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d("SunshineIntentService", "About to handle incoming intent...");
+        if (intent != null) {
+            String action = intent.getAction();
+            if (action != null && action.equals(ACTION_REFRESH_DATA_FROM_SOURCE)) {
+                Log.d("SunshineIntentService", "Intent action is to refresh data! Sending command to refresh...");
+                SunshineSyncTask.syncWeather(this.getBaseContext());
+            }
+        }
+        Log.d("SunshineIntentService", "Incoming intent has been handled, service is now stopping...");
+    }
+}
--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,39 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+//  DONE (1) Create a class called SunshineSyncTask
+//  DONE (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+//      DONE (3) Within syncWeather, fetch new weather data
+//      DONE (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
+
+public class SunshineSyncTask {
+    synchronized public static void syncWeather(Context context) {
+        Log.d("SunshineSyncTask", "About to begin syncWeather...");
+        URL weatherUrl = NetworkUtils.getUrl(context);
+        String jsonWeatherResponse = "";
+        ContentValues[] weatherValues = null;
+        try {
+            jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherUrl);
+            weatherValues = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (weatherValues != null) {
+            ContentResolver cr = context.getContentResolver();
+            cr.delete(WeatherContract.WeatherEntry.CONTENT_URI,
+                    null,
+                    null);
+            cr.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI,
+                    weatherValues);
+        }
+    }
+}
